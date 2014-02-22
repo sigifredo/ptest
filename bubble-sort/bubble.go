d00fdff26e12d57a2ac5f42c20a92f82837cb9fa
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-22 12:11:54 -0500

    Eliminamos linea en blanco.

--- bubble-sort/bubble.go
+++ bubble-sort/bubble.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func bubble(arr []int) {
     for i := 0; i < len(arr); i++ {
-
         for j := 0; j < len(arr)-1; j++ {
             if arr[j] > arr[j+1] {
                 arr[j] = arr[j] + arr[j+1]
