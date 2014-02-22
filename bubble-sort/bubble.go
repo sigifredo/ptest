d01e66d6e6daf1bf785d0a01279bfb932554f4ad
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-22 12:10:24 -0500

    Agregamos archivo de compilación y algoritmo de burbuja en Go.

---
+++ bubble-sort/bubble.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func bubble(arr []int) {
+    for i := 0; i < len(arr); i++ {
+
+        for j := 0; j < len(arr)-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j] = arr[j] + arr[j+1]
+                arr[j+1] = arr[j] - arr[j+1]
+                arr[j] = arr[j] - arr[j+1]
+            }
+        }
+    }
+}
+
+func main() {
+    var n int
+    var arr []int
+
+    fmt.Scanf("%d", &n)
+    arr = make([]int, n)
+
+    for i, _ := range arr {
+        fmt.Scanf("%d", &arr[i])
+    }
+
+    bubble(arr)
+
+    for _, v := range arr {
+        fmt.Printf("%d ", v)
+    }
+    fmt.Printf("\n")
+}
