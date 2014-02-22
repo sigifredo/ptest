d4b3b8a74602a296fdad3f229243388e3c23164f
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-21 21:54:05 -0500

    Agregamos algoritmo de ordenamiento en C.

---
+++ bubble-sort/bubble.c
@@ -0,0 +1,43 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble(int * arr, int n)
+{
+    int i, j;
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < n-2; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int aux = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = aux;
+                // arr[j] = arr[j] + arr[j+1];
+                // arr[j+1] = arr[j] - arr[j+1];
+                // arr[j] = arr[j] - arr[j+1];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int i, n;
+    int * arr;
+
+    scanf("%d", &n);
+
+    arr = (int*) malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++)
+        scanf("%d", arr+i);
+
+    bubble(arr, n);
+
+    for (i = 0; i < n-1; i++)
+        printf("%d,", arr[i]);
+    printf("%d\n", arr[i]);
+}
