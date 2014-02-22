78a83fa2afd2fed442236d6f87fc06cb6e8a4715
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-21 21:57:52 -0500

    Mejoramos el algoritmo eliminando varible auxiliar.

--- bubble-sort/bubble.c
+++ bubble-sort/bubble.c
@@ -6,18 +6,15 @@
 void bubble(int * arr, int n)
 {
     int i, j;
-    for (i = 1; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n-2; j++)
+        for (j = 0; j < n-1; j++)
         {
             if (arr[j] > arr[j+1])
             {
-                int aux = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
-                // arr[j] = arr[j] + arr[j+1];
-                // arr[j+1] = arr[j] - arr[j+1];
-                // arr[j] = arr[j] - arr[j+1];
+                arr[j] = arr[j] + arr[j+1];
+                arr[j+1] = arr[j] - arr[j+1];
+                arr[j] = arr[j] - arr[j+1];
             }
         }
     }
