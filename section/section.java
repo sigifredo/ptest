a19838f504ab1d05d2c311f5aa2724fc623109de
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-23 13:36:31 -0500

    Agregamos algoritmo Section Sort en Java.

---
+++ section/section.java
@@ -0,0 +1,54 @@
+/**
+ * Author: Sigifredo Escobar Gómez
+ * Email: sigifredo89@gmail.com
+ * Date: Sun, 23 Feb 2014 13:35:35 -0500
+ * Algorithm: Section Sort
+ */
+
+import java.util.Scanner;
+
+
+class section
+{
+
+    public static void section(int[] arr)
+    {
+        int i, j, minimum;
+
+        for (i = 0; i < arr.length-1; i++)
+        {
+            minimum = i;
+
+            for (j = i+1; j < arr.length; j++)
+                if (arr[j] < arr[minimum])
+                    minimum = j;
+
+            if (minimum != i)
+            {
+                arr[i] = arr[i] + arr[minimum];
+                arr[minimum] = arr[i] - arr[minimum];
+                arr[i] = arr[i] - arr[minimum];
+            }
+        }
+    }
+
+    public static void main(String[] ar)
+    {
+        int n;
+        int [] arr;
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = scan.nextInt();
+
+        section(arr);
+
+        for (int i = 0; i < n-1; i++)
+            System.out.print("" + arr[i] + ",");
+        System.out.println("" + arr[arr.length-1]);
+    }
+
+}
