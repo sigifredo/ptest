4c7332f0968dfdacd61dc4f0909d3ec4e66e3d12
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-23 17:47:54 -0500

    Quitamos ; (punto y coma) al final de la liena.

--- bubble/bubble.py
+++ bubble/bubble.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Sigifredo Escobar Gómez
 # Email: sigifredo89@gmail.com
-# Date: Sat, 22 Feb 2014 16:39:47 -0500
+# Date: Sun, 23 Feb 2014 17:47:33 -0500
 # Algorithm: Bubble Sort
 
 
@@ -11,9 +11,9 @@ def bubble(arr):
     for i in range(n):
         for j in range(n-1):
             if arr[j] > arr[j+1]:
-                arr[j] = arr[j] + arr[j+1];
-                arr[j+1] = arr[j] - arr[j+1];
-                arr[j] = arr[j] - arr[j+1];
+                arr[j] = arr[j] + arr[j+1]
+                arr[j+1] = arr[j] - arr[j+1]
+                arr[j] = arr[j] - arr[j+1]
 
 
 if __name__ == '__main__':
