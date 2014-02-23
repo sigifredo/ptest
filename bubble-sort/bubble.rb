79f73039439bc45853aad40874c2c9e754dfe557
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2014-02-23 12:23:09 -0500

    Eliminamos ; (punto y coma) del código en Ruby.

--- bubble-sort/bubble.rb
+++ bubble-sort/bubble.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby
 # Author: Sigifredo Escobar Gómez
 # Email: sigifredo89@gmail.com
-# Date: Sat, 22 Feb 2014 16:39:47 -0500
+# Date: Sun, 23 Feb 2014 12:22:50 -0500
 # Algorithm: Bubble Sort
 
 def bubble(arr)
     for i in 0...arr.length
         for j in 0...arr.length-1
             if (arr[j] > arr[j+1])
-                arr[j] = arr[j] + arr[j+1];
-                arr[j+1] = arr[j] - arr[j+1];
-                arr[j] = arr[j] - arr[j+1];
+                arr[j] = arr[j] + arr[j+1]
+                arr[j+1] = arr[j] - arr[j+1]
+                arr[j] = arr[j] - arr[j+1]
             end
         end
     end
